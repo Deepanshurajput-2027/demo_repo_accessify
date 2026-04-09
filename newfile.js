310f147afb69fec801d5062867872b456d692ac9
Deepanshu rajput <deepanshurajput2731@gmail.com>
2026-04-09 15:50:32 +0530

    UI changes

--- newfile.js
+++ newfile.js
@@ -4,6 +4,16 @@
     <i class="icon-save"></i>
   </button>
   <input type="text" placeholder="Enter name" />
-    <img src="profile.jpg" />
+  <section>
+    <div class="card">
+  <img src="profile.jpg" />
+  <button style="background: white; color: lightgray;" onclick="save()">
+    <i class="icon-save"></i>
+  </button>
+  <input type="text" placeholder="Enter name" />
    
 </div>
+  </section>
+   
+</div>
+
--- other.js
+++ other.js
@@ -12,15 +12,6 @@
   <section>
     <div class="card">
   <img src="profile.jpg" />
-  <button style="background: white; color: lightgray;" onclick="save()">
-    <i class="icon-save"></i>
-  </button>
-  <input type="text" placeholder="Enter name" />
-    <img src="profile.jpg" />
-  <button style="background: white; color: lightgray;" onclick="save()">
-    <i class="icon-save"></i>
-  </button>
-  <input type="text" placeholder="Enter name" />
 </div>
 </section>
 </div>
