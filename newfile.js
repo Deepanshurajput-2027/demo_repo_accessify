e14b90a6689cf748620d1c87a3ddc8ea0e103d0b
Deepanshu rajput <deepanshurajput2731@gmail.com>
2026-04-09 14:53:10 +0530

    first commit

---
+++ newfile.js
@@ -0,0 +1,7 @@
+<div class="card">
+  <img src="profile.jpg" />
+  <button style="background: white; color: lightgray;" onclick="save()">
+    <i class="icon-save"></i>
+  </button>
+  <input type="text" placeholder="Enter name" />
+</div>
