906af5f8dae47ba84324762c89af255e2c7ad0fe
DEEPANSHU RAJPUT <deepanshurajput2731@gmail.com>
2026-04-09 15:50:58 +0530

    🚀 [Accessify] Auto-remediated accessibility issues (3 files)
    Fixed accessibility violations in:
    - index.html
    - newfile.js
    - other.js

--- newfile.js
+++ newfile.js
@@ -1,9 +1,18 @@
-<div class="card">
-  <img src="profile.jpg" />
-  <button style="background: white; color: lightgray;" onclick="save()">
-    <i class="icon-save"></i>
+<div className="card">
+  <img src="profile.jpg" alt="User profile picture" />
+  <button style={{ background: 'white', color: 'lightgray' }} onClick={save} aria-label="Save profile">
+    <i className="icon-save"></i>
   </button>
-  <input type="text" placeholder="Enter name" />
-    <img src="profile.jpg" />
+  <input type="text" placeholder="Enter name" aria-label="Enter user name" />
+  <section>
+    <div className="card">
+  <img src="profile.jpg" alt="User profile picture" />
+  <button style={{ background: 'white', color: 'lightgray' }} onClick={save} aria-label="Save profile">
+    <i className="icon-save"></i>
+  </button>
+  <input type="text" placeholder="Enter name" aria-label="Enter user name" />
+   
+</div>
+  </section>
    
 </div>
