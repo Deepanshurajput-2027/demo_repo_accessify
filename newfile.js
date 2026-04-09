80e09812418e30b67e55eaa6410cba0bb7eff7c5
Deepanshu rajput <deepanshurajput2731@gmail.com>
2026-04-09 15:12:31 +0530

    new change

--- newfile.js
+++ newfile.js
@@ -5,8 +5,5 @@
   </button>
   <input type="text" placeholder="Enter name" />
     <img src="profile.jpg" />
-  <button style="background: white; color: lightgray;" onclick="save()">
-    <i class="icon-save"></i>
-  </button>
-  <input type="text" placeholder="Enter name" />
+   
 </div>
--- other.js
+++ other.js
@@ -9,4 +9,20 @@
     <i class="icon-save"></i>
   </button>
   <input type="text" placeholder="Enter name" />
+  <section>
+    <div class="card">
+  <img src="profile.jpg" />
+  <button style="background: white; color: lightgray;" onclick="save()">
+    <i class="icon-save"></i>
+  </button>
+  <input type="text" placeholder="Enter name" />
+    <img src="profile.jpg" />
+  <button style="background: white; color: lightgray;" onclick="save()">
+    <i class="icon-save"></i>
+  </button>
+  <input type="text" placeholder="Enter name" />
 </div>
+</section>
+</div>
+
+
