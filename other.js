a69c889d8001f859666b266eacfe460ea89f4441
Deepanshu rajput <deepanshurajput2731@gmail.com>
2026-06-06 12:39:48 +0530

    chore: testing accessify e2e pipeline

--- other.js
+++ other.js
@@ -4,7 +4,7 @@
     <i className="icon-save"></i>
   </button>
   <input type="text" placeholder="Enter name" />
-  <img src="profile.jpg" alt="User profile picture" />
+  <img src="profile.jpg" />
   <button style={{ background: 'white', color: 'lightgray' }} onClick={save} aria-label="Save profile">
     <i className="icon-save"></i>
   </button>
